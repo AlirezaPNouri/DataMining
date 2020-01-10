d0782c5d72c74302598d120cfedb34752fbc102c
alirezaPNouri <apashamoham@miners.utep.edu>
2020-01-10 13:42:21 -0700

    create a diabetes classification project by KNN

---
+++ Codes/KNN/KNN_classification.py
@@ -0,0 +1,27 @@
+# This code explains and elaborates the KNN classification.
+# Author: Alireza P. Nouri
+
+import pandas as pd
+import sklearn
+
+dataset_diabetes = pd.read_csv('../../DataSet/DiabetesDataset')
+
+# To get the size of the database and check the values in the dataset
+print(dataset_diabetes.shape)
+print(dataset_diabetes.head())
+
+# To split the data into training, validation, and testing set
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('Program runs without any problem')
